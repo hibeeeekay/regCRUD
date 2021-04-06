0740f7c1948e5e65f50dc9c3a94144ff0b52a2bf
hibeeeekay <60797412+hibeeeekay@users.noreply.github.com>
2021-04-06 04:53:09 -0700

    Update constants.php

--- constants.php
+++ constants.php
@@ -1,5 +1,10 @@
 <?php
-define('DB_HOST', 'remotemysql.com');
-define('DB_USER', 'r3h2dtMIhE');
-define('DB_PASS', '992AANkodJ');
-define('DB_NAME', 'r3h2dtMIhE');
+//define('DB_HOST', 'remotemysql.com');
+//define('DB_USER', 'r3h2dtMIhE');
+//define('DB_PASS', '992AANkodJ');
+//define('DB_NAME', 'r3h2dtMIhE');
+
+define('DB_HOST', '127.0.0.1');
+define('DB_USER', 'root');
+define('DB_PASS', 'fc8dhppU]hkW9D!n');
+define('DB_NAME', 'user-verification');
