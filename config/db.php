d2172ad538f7ae87ffb0885cde135b256cd1f93e
hibeeeekay <60797412+hibeeeekay@users.noreply.github.com>
2021-04-06 04:12:20 -0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 require 'constants.php';
 
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS);
 
 if($conn->connect_error){
     die('Database error:' . $conn->connect_error);
