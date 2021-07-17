d4ddbbf621fdb07b7ca52af09dc3fdee5b3a8dec
hibeeeekay <60797412+hibeeeekay@users.noreply.github.com>
2021-07-17 23:27:05 +0100

    Update constants.php

--- constants.php
+++ constants.php
@@ -4,6 +4,9 @@ define('DB_USER', 'r3h2dtMIhE');
 define('DB_PASS', '992AANkodJ');
 define('DB_NAME', 'r3h2dtMIhE');
 
+define('EMAIL', 'hibeeeekay@gmail.com');
+define('PASSWORD', 'incrediblechoice');
+
 //define('DB_HOST', '127.0.0.1');
 //define('DB_USER', 'root');
 //define('DB_PASS', 'fc8dhppU]hkW9D!n');
